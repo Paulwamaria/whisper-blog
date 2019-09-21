375645566955793b1d01e782218f0bee80e17b35
Paulwamaria <paulwamaria@gmail.com>
2019-09-21 17:03:59 +0300

    Add more styles

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,34 @@
 from . import main
-from flask import render_template,redirect, url_for
+from flask import render_template, redirect, url_for
+posts = [
+    {
+        'author': 'Paul Wamaria',
+        'title': 'Blog Post1',
+        'content': 'First Post Content',
+        'date_posted': 'September 20, 2019'
+    },
+    {
+        'author': 'Wincott Wamaria',
+        'title': 'Blog Post2',
+        'content': 'Second Post Content',
+        'date_posted': 'September 21, 2019'
+    }
 
+]
 
 @main.route('/')
 def index():
-    message="Hello everyone, welcome to whisper blogers"
+    message = "Welcome to Whisper Blogers"
 
-    return render_template('index.html',message=message)
+
+
+    return render_template('index.html', message=message, posts=posts)
+
 
 @main.route('/about')
-def about():
-    about_message="We post anything that comes in mind"
+def about(): 
 
-    return render_template('about.html',about_message=about_message)
+
+    about_message = "We post anything that promotes mental health"
+
+    return render_template('about.html', about_message=about_message)
