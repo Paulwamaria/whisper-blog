2a45fb06b35780dd9d625541fd3f6396c8f17b78
Paulwamaria <paulwamaria@gmail.com>
2019-09-22 09:47:30 +0300

    Add a secret Key and a database uri

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+import os
+
 class Config:
-    pass
+    SECRET_KEY='0aea274cb6d66959e329887208e3365a'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://paul:leejones1@localhost/blog'
 
 
 class ProdConfig(Config):
