315d7c6bd6e7521f98eef7c983ed58fd36add68e
Paulwamaria <paulwamaria@gmail.com>
2019-09-22 09:48:09 +0300

    initialize login manager and db

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,30 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from config import config_options
+from flask_login import LoginManager
 
 
-
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
+db=SQLAlchemy()
+
 
 def create_app():
-    app=Flask(__name__)
-
+    app = Flask(__name__)
 
     app.config.from_object(config_options['development'])
     bootstrap.init_app(app)
+    login_manager.init_app(app)
+    db.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
+    app.register_blueprint(auth_blueprint,)
 
     return app
