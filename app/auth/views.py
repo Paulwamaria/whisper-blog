674296db6c4da60fc231fdbe9fd94aaa18ddbff5
Paulwamaria <paulwamaria@gmail.com>
2019-09-24 10:55:29 +0300

    Add display to random quotes

--- app/auth/views.py
+++ app/auth/views.py
@@ -97,4 +97,40 @@ def new_post():
         db.session.commit()
         flash('Your post has been created','success')
         return redirect(url_for('main.index'))
-    return render_template('create_post.html', title =' New Post',post_form=form)
+    return render_template('create_post.html', title =' New Post',post_form=form, legend='New post')
+
+@auth.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html',post=post, title = post.title) 
+
+
+@auth.route('/post/<int:post_id>/update',methods= ['GET','POST'])
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your post has been updated","success")
+        return redirect(url_for('auth.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html',post=post, legend = 'Update Post',post_form=form) 
+
+
+@auth.route('/post/<int:post_id>/delete',methods= ['GET','POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been delete succesifuly','success')
+
+    return redirect(url_for('main.index'))
