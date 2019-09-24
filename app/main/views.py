17945332cb1b7333bfe187ce4e1ba0ef37523784
Paulwamaria <paulwamaria@gmail.com>
2019-09-24 10:54:55 +0300

    Added the funtions to update and delete posts

--- app/main/views.py
+++ app/main/views.py
@@ -1,16 +1,20 @@
 from . import main
-from ..models import User,Post
+import requests
+from ..models import User,Post,Quote
 from flask_login import login_required
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for,request
 
 
 @main.route('/')
 def index():
     message = "Welcome to Whisper Blogers"
-
-    posts=Post.query.all()
-
-    return render_template('index.html', message=message, posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts=Post.query.order_by(Post.date_posted.desc()).paginate(page=page,per_page=2) 
+    quote_data = requests.get('http://quotes.stormconsultancy.co.uk/random.json' ).json()
+    quote_content= quote_data.get('quote')
+    quote_author= quote_data.get('author')
+    
+    return render_template('index.html', message=message, posts=posts,quote=quote_content,author=quote_author)
 
 
 @main.route('/about')
@@ -20,3 +24,15 @@ def about():
     about_message = "We post anything that promotes mental health"
 
     return render_template('about.html', about_message=about_message)
+
+@main.route('/user/<string:username>')
+def user_posts(username):
+ 
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts=Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+            .paginate(page=page,per_page=2) 
+
+    return render_template('user_post.html', posts=posts, user=user)
+
